00948ff44adae9dd0e25606270210ff787c514e7
bactoria <bactoria@gmail.com>
2020-06-06 12:43:08 +0900

    불필요한 웹팩 설정 제거

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,2 @@
 module.exports = {
-    configureWebpack: {
-        output: {
-            filename: '[name].js',
-            publicPath: '/webpack/'
-        },
-        module: {
-            loaders: [
-                { test: /\.css$/, loader: 'style!css?sourceMap' },
-                { test: /\.scss$/, loaders: ['style', 'css?sourceMap', 'sass'] },
-                { test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url' },
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules)/,
-                    loader: 'babel',
-                    query: {
-                        presets: ['es2015', 'stage-2']
-                    }
-                }
-            ]
-        },
-    }
 }
